719ee5f47e8496a09fed931be8f39bd0f3a21f97
oktobat <tsalt@hanmail.net>
2024-07-01 12:31:00 +0900

    7/1일 12시 30분

--- src/TodoInsert.jsx
+++ src/TodoInsert.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components'
+
+const TodoInsertBlock = styled.div``
 
 const TodoInsert = () => {
+  // 컴포넌트 내부에서 변경되는 데이터 관리 : state 변수
+  const [text, setText] = useState("")
+
+  const onChange = (e)=>{
+    console.log(e.target.value)
+    setText(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
-    </div>
+    <TodoInsertBlock>
+      <form onSubmit={ onSubmit }>
+        <input type="text" placeholder="할 일을 입력하세요." value={text} onChange={ onChange } />
+        <button type="submit">전송</button>
+      </form>
+    </TodoInsertBlock>
   );
 };
 

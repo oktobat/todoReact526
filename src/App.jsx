f0e96430241e7577d1e923b4e8618d4f0064f5c9
oktobat <tsalt@hanmail.net>
2024-07-01 12:04:16 +0900

    7/1일 12시 4분

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import TodoTemplate from '@/TodoTemplate'
 import TodoHeader from '@/TodoHeader'
+import TodoInsert from '@/TodoInsert'
+import TodoList from '@/TodoList'
+import TodoFooter from '@/TodoFooter'
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoHeader />
-      
+      <TodoInsert />
+      <TodoList />
+      <TodoFooter />
     </TodoTemplate>
   );
 };
---
+++ src/TodoFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoFooter = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default TodoFooter;
